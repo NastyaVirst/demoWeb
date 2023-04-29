8ab0f316dcb033fe0ecbdd0bfee96cff6c73d61f
nasya <nastua5.1991@mail.ru>
2023-04-29 17:44:08 +0500

    init

---
+++ src/main/java/com/example/demoWeb/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demoWeb.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+//@Configuration
+public class SecurityConfig {
+
+  /*  @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();*/
+  //  }
+
+}
+
