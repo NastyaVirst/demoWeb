d43a1bac2c9aade1d2afc69932d87f515b714902
nasya <nastua5.1991@mail.ru>
2023-04-29 17:18:35 +0500

    init

--- src/main/java/com/example/demoWeb/controllers/ControllerNomenclature.java
+++ src/main/java/com/example/demoWeb/controllers/ControllerNomenclature.java
@@ -1,10 +1,12 @@
 package com.example.demoWeb.controllers;
 
+import ch.qos.logback.classic.Logger;
 import com.example.demoWeb.entities.EntityNomenclature;
-import com.example.demoWeb.entities.EntityWagon;
 import com.example.demoWeb.services.ServicesNomenclature;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +17,10 @@ import java.util.List;
 
 @RestController
 @Tag(name = "Номенклатура", description = "Информация по номенклатуре")
+/*@SecurityRequirement(name = "user")*/
 public class ControllerNomenclature {
     ServicesNomenclature servicesNomenclature;
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(ControllerNomenclature.class);
 
     public ControllerNomenclature(ServicesNomenclature servicesNomenclature) {
         this.servicesNomenclature = servicesNomenclature;
@@ -24,6 +28,7 @@ public class ControllerNomenclature {
     @GetMapping(value = "/nomenclature", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Получить информацию по всей номенклатуре")
     public ResponseEntity<List<EntityNomenclature>> getAllNomenclature() {
+        logger.info("get info about nomenclature");
         List<EntityNomenclature> listNomenclature = servicesNomenclature.readNomenclature();
         if (listNomenclature.isEmpty()) {
             throw new ResponseStatusException(
@@ -45,10 +50,12 @@ public class ControllerNomenclature {
         return new ResponseEntity<>(servicesNomenclature.updateNomenclature(id, entityNomenclature), HttpStatus.OK);
     }
 
-    @DeleteMapping("/nomenclature/del/{id}")
+    @DeleteMapping("/nomenclature/{id}")
     @Operation(summary = "Удалить номенклатуру")
-    public ResponseEntity<Long> deleteNomenclature(@PathVariable Long id) {
-        return new ResponseEntity<>(servicesNomenclature.deleteNomenclature(id),HttpStatus.OK);
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK, reason = "OK")
+    public Long deleteNomenclature(@PathVariable Long id) {
+        return servicesNomenclature.deleteNomenclature(id);
     }
 
 }
